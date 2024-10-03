bb3dc05913e48341ebfb9252c491ae5621073bf6
Alexander Chertovkikh <alexandr.operman@gmail.com>
2024-10-03 22:11:35 +0300

    Настройка конфигов еслинта и преттир

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,28 +1,43 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+    { ignores: ['dist'] },
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+            'no-console': ['error', { allow: ['warn', 'error'] }],
+            quotes: ['error', 'single', { avoidEscape: true }],
+            semi: 'error',
+            curly: 'error',
+            'no-mixed-spaces-and-tabs': 'error',
+            'max-len': [
+                'warn',
+                {
+                    code: 100,
+                    ignoreTemplateLiterals: true,
+                    tabWidth: 4,
+                    ignoreStrings: true,
+                    ignoreRegExpLiterals: true,
+                    ignoreComments: true,
+                },
+            ],
+            'object-curly-spacing': ['error', 'always'],
+            'space-in-parens': ['error', 'never'],
+        },
+    }
+);
